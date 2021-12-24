6bf54bb7932c5559483cd6c00f5769b4b7ba88dc
Julian Grisales <jgrisales@gmail.com>
2021-12-24 13:27:24 +0100

    Add Feedback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { v4 as uuidv4 } from "uuid"
 import Header from "./components/Header"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackList from "./components/FeedbackList"
@@ -8,16 +9,22 @@ import FeedbackForm from "./components/FeedbackForm"
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData)
 
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
   const deleteFeedback = id => {
     if (window.confirm("Are you sure you want to delete?")) {
       setFeedback(feedback.filter((item) => item.id != id))
     }
   }
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -9,7 +9,7 @@ const FeedbackList = ({ feedback, handleDelete }) => {
   return (
     <div className="feedback-list">
       {feedback.map(item => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   )
@@ -18,7 +18,7 @@ const FeedbackList = ({ feedback, handleDelete }) => {
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
       text: PropTypes.string.isRequired,
       rating: PropTypes.number.isRequired
     })
