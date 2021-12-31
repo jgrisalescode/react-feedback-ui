6a050117c728630763f87ab02d016045d7b8a726
Julian Grisales <jgrisales@gmail.com>
2021-12-31 11:02:58 +0100

    Edit Feedback Event

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,17 +1,20 @@
 import { useContext } from "react"
-import { FaTimes } from "react-icons/fa"
+import { FaTimes, FaEdit } from "react-icons/fa"
 import PropTypes from "prop-types"
 import Card from "./shared/Card"
 import FeedbackContext from "../context/FeedbackContext"
 
 const FeedbackItem = ({ item }) => {
-  const { deleteFeedback } = useContext(FeedbackContext)
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext)
 
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
       <button className="close">
         <FaTimes onClick={() => deleteFeedback(item.id)} color="purple" />
+      </button>
+      <button className="edit">
+        <FaEdit onClick={() => editFeedback(item)} color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
