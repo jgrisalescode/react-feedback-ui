484e50f5613b35facec2860ed58a52d3de35bb71
Julian Grisales <jgrisales@gmail.com>
2022-01-02 15:38:53 +0100

    Update & Delete From JSON-Server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -37,17 +37,28 @@ export const FeedbackProvider = ({ children }) => {
     }
 
     // Delete feedback
-    const deleteFeedback = id => {
+    const deleteFeedback = async (id) => {
         if (window.confirm("Are you sure you want to delete?")) {
+            await fetch(`/feedback/${id}`, { method: 'DELETE' })
             setFeedback(feedback.filter((item) => item.id != id))
         }
     }
 
     // Update feedback
-    const updateFeedback = (id, updatedItem) => {
+    const updateFeedback = async (id, updatedItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedItem)
+        })
+
+        const data = await response.json()
+
         setFeedback(
             feedback.map(item => (
-                item.id === id ? { ...item, ...updatedItem } : item
+                item.id === id ? { ...item, ...data } : item
             ))
         )
     }
